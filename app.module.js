f129de857e0039fafa057e361a455fac36c9f5cf
Vinicius Silvano <viniciusssf3@gmail.com>
2019-10-22 20:03:02 -0300

    adicionando novas funcionalidades preparando para corrigir erros

--- app.module.js
+++ app.module.js
@@ -1,6 +1,8 @@
 // Define the 'phonecatApp' module
 angular.module('phonecatApp', [
     // ...which depends on the 'phoneList' module
+    'ngRoute',
+    'phoneDetail',
     'phoneList',
     'phones'
 ]);
