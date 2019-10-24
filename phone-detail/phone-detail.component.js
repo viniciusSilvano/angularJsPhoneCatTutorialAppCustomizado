bfe26efbf00afd7e45541ed61da1151b132f8e51
Vinicius Silvano <viniciusssf3@gmail.com>
2019-10-23 23:00:55 -0300

    falta arrumar algumas documentações

--- phone-detail/phone-detail.component.js
+++ phone-detail/phone-detail.component.js
@@ -1,10 +1,15 @@
 angular
 .module('phoneDetail')
 .component('phoneDetail',{
-    template: 'TBD: Detail view for <span>{{$ctrl.phoneId}}</span>',
-    controller: ['$routeParams',
-        function PhoneDetailController($routeParams){
-            this.phoneId = $routeParams.phoneId;
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: ['$http','$routeParams',
+        function PhoneDetailController($http,$routeParams){
+           var self = this;
+            self.phone = 
+           $http.get('http://localhost:5000/#!/phones/' + $routeParams.phoneId +
+           '.json').then(function(response){
+                self.phone = response.data;
+           });
         }
     ]
 });
