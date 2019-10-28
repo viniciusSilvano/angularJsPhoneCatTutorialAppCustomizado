d3c9386a9becc5ec85cb13b06ee745fe7136282b
Vinicius Silvano <viniciusssf3@gmail.com>
2019-10-27 22:04:26 -0300

    adicionando novo teste

--- teste/teste.component.js
+++ teste/teste.component.js
@@ -6,6 +6,28 @@ angular
         var self = this;
         self.titulo = 'teste';
         console.log($route.current.css);
-        $scope.css = $route.current.css;
+        self.css = $route.current.css;
+        self.mostrarContainer = true;
+        self.mostrar = false;
+        self.everywhere = angular.element(window.document);
+        console.log(self.mostrar);
+        self.MostrarToolTip = function (valor){
+            console.log('entrou');
+            if(self.mostrar === true){
+                self.mostrar = false;
+            }else{
+                self.mostrar = true;
+            }
+            $scope.$apply;
+        }
+        /*self.everywhere.bind('click', function(event){
+            console.log(angular.element(document).find('#tooltipText'));
+
+            if(self.mostrar === true && !isButtonClick){
+                self.mostrar = false;
+                $scope.$apply();
+            }
+        });*/
+        
     }]
 });
