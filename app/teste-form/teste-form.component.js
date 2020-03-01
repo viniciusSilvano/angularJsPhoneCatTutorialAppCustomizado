d401a22c346ecf452d8191d14512765123d33949
Vinicius Silvano <viniciusssf3@gmail.com>
2020-03-01 18:44:11 -0300

    removendo comentário

--- app/teste-form/teste-form.component.js
+++ app/teste-form/teste-form.component.js
@@ -4,7 +4,6 @@
         controllerAs: 'testeFormController',
         templateUrl:'app/teste-form/teste-form.template.html',
         controller: ['$scope', function testeFormController($scope){
-            // regex que valida email: ([a-z])(@)(.*[a-z])(\.com$)
             const testeFormController = this;
             testeFormController.master = {}
             testeFormController.update = function(user) {
