8ca0d3f32ded89cda109385d8206c5d97c38ed9d
Vinicius <viniciusssf3@gmail.com>
2020-03-10 19:34:49 -0300

    realizando modificações teste polyline

--- app/teste-polyline/teste-polyline.component.js
+++ app/teste-polyline/teste-polyline.component.js
@@ -8,12 +8,47 @@
             const vm = this;
             vm.zoomIn = zoomIn;
             vm.zoomOut = zoomOut;
-            vm.points = "10,10,20,0, 30,0, 30,0, 40,0, 40,0, 50,0, 50,0, 60,10, 120,0, 360,0, 1280,0";
+            vm.points = "10,10 20,0 30,0 30,0 40,0 40,0 50,0 50,0 60,10 120,0 360,0 1280,0";
+            vm.tamanhoEixoXPolyline = 0;
+            vm.tamanhoEixoYPolyline = 0;
             vm.svgStyle = {
                 left: '10%'
             }
-            
-            //vm.points3 = vm.points2.toString().split(',');
+            const pontosCarteseanos={
+                x: 0,
+                y: 1,
+                z: 2,
+            }
+
+            inicializarTamanhoEixoXPolyline();
+            inicializarTamanhoEixoYPolyline();
+
+            function inicializarTamanhoEixoXPolyline(){
+                vm.tamanhoEixoXPolyline = 0;
+                recuperarPonto(pontosCarteseanos.x);
+            }
+
+            function inicializarTamanhoEixoYPolyline(){
+                vm.tamanhoEixoYPolyline = 0;
+                recuperarPonto(pontosCarteseanos.y);
+            }
+
+            function recuperarPonto(valorPonto){
+                let paresPontos = vm.points.split(' ');
+                paresPontos.forEach(ponto => {
+                    let pontos = ponto.split(',');
+                    switch(valorPonto){
+                        case 0:
+                            vm.tamanhoEixoXPolyline += parseInt(pontos[0]);
+                            break;
+                        case 1:
+                            vm.tamanhoEixoYPolyline += parseInt(pontos[1]);
+                            break;
+                        case 2:
+                            break;
+                    }
+                });
+            }
 
             function zoomIn(){
                 realizarZoom('ZoomIn')
