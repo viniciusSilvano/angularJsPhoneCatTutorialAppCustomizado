bc8a57a84df39fc6200d9faa515ae25731a5f96c
Vinicius Silvano <viniciusssf3@gmail.com>
2020-03-14 21:41:04 -0300

    adicionando a requisição do projeto teste java.

--- app/teste-java-project/livro/livro.component.js
+++ app/teste-java-project/livro/livro.component.js
@@ -4,6 +4,25 @@
     .module("phonecatApp.testeJavaProject.livro")
     .component("phonecatApp.testeJavaProject.livro", {
       templateUrl: "app/teste-java-project/livro/livro.template.html",
-      controller: ["$scope", function livroController() {}]
+      controller: [
+        "$scope",
+        "$http",
+        function livroController($scope, $http) {
+          const vm = this;
+
+          init();
+
+          function init() {
+            $http
+              .get("http://localhost:8080/javaTeste/livro")
+              .then(response => {
+                console.log("livros: ", response);
+              }),
+              error => {
+                console.log("Erro ao recuperar livros");
+              };
+          }
+        }
+      ]
     });
 })();
