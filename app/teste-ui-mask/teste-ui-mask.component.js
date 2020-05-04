4f6ab529fcc0c2538b22c5bf84a0cac79c3e01e9
Vinícius Silvano Silveira Felipe <vinicius.vssf@pf.gov.br>
2020-05-04 18:03:48 -0300

    adicionando teste de ui-mask

---
+++ app/teste-ui-mask/teste-ui-mask.component.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+    angular
+      .module("phonecatApp.testeUIMask")
+      .component("phonecatApp.testeUIMask", {
+        templateUrl: "app/teste-ui-mask/teste-ui-mask.template.html",
+        controller: ["$scope", function testeUIMaskController($scope) {
+            
+        }]
+      });
+  })();
+  
